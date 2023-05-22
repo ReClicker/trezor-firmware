a66affb29c605dae7ff34b696a4233d4fba9f992
grdddj <jiri.musil06@seznam.cz>
2023-05-22 17:19:36 +0200

    WIP - get rid of #[cfg(feature = "micropython")]

--- core/embed/rust/src/ui/model_tr/component/input_methods/mod.rs
+++ core/embed/rust/src/ui/model_tr/component/input_methods/mod.rs
@@ -1,13 +1,8 @@
 pub mod choice;
 pub mod choice_item;
 
-#[cfg(feature = "micropython")]
 pub mod number_input;
-#[cfg(feature = "micropython")]
 pub mod passphrase;
-#[cfg(feature = "micropython")]
 pub mod pin;
-#[cfg(feature = "micropython")]
 pub mod simple_choice;
-#[cfg(feature = "micropython")]
 pub mod wordlist;
